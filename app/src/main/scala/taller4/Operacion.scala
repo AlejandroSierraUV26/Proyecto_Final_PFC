e84eb9d7cdc53ea8d0595efa94fc382fec0a314c
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-12-16 20:34:30 -0500

    Soluciones paraleas incoporadas

--- app/src/main/scala/taller4/Operacion.scala
+++ app/src/main/scala/taller4/Operacion.scala
@@ -1,41 +1,33 @@
 package taller4
 
+import common.task
+import scala.util.Random
+import scala.annotation.tailrec
+
 object Operacion {
-  // Se implementa la función que se pide en el taller
   val alfabeto = Seq('a', 'c', 'g', 't')
   type Oraculo = Seq[Char] => Boolean
-
   def generarCadenas(n: Int): Seq[Seq[Char]] = {
-    def generarCombinaciones(tamaño: Int): Seq[Seq[Char]] = {
-      if (tamaño == 0) {
-        Seq(Seq())
-      } else {
-        val combinacionesMenores = generarCombinaciones(tamaño - 1)
-        for {
-          cadena <- combinacionesMenores
-          letra <- alfabeto
-        } yield letra +: cadena
-      }
+  def generarCombinaciones(tamaño: Int): Seq[Seq[Char]] = {
+    if (tamaño == 0) {
+      Seq(Seq())
+    } else {
+      val combinacionesMenores = generarCombinaciones(tamaño - 1)
+      for {
+        cadena <- combinacionesMenores
+        letra <- alfabeto
+      } yield(letra +: cadena)
     }
-    generarCombinaciones(n)
   }
+  generarCombinaciones(n)
+}
   def reconstuirCadenaIngenuo(n: Int, o: Oraculo): Seq[Char] = {
     val cadenas = generarCadenas(n)
-
-    def buscarCadena(cadenasRestantes: Seq[Seq[Char]]): Seq[Char] = {
-      if (cadenasRestantes.isEmpty) {
-        Seq()
-      } else {
-        val actual = cadenasRestantes.head
-        if (o(actual)) {
-          actual
-        } else {
-          buscarCadena(cadenasRestantes.tail)
-        }
-      }
-    }
-
-    buscarCadena(cadenas)
+    cadenas.find(o).getOrElse(Seq())
+  }
+  def reconstuirCadenaIngenuoPar(n: Int, o: Oraculo): Seq[Char] = {
+    val cadenas = task(generarCadenas(n)).join()
+    cadenas.find(o).getOrElse(Seq())
   }
   def reconstruirCadenaMejorado(n: Int, o: Oraculo): Seq[Char] = {
     def reconstruirRecursivo(actual: Seq[Char], longitudActual: Int): Seq[Char] = {
@@ -43,6 +35,7 @@ object Operacion {
         actual
       } else if (longitudActual < n) {
         val siguientes = alfabeto.flatMap(letra => reconstruirRecursivo(actual :+ letra, longitudActual + 1))
+
         siguientes
       } else {
         Seq()
@@ -51,107 +44,108 @@ object Operacion {
 
     reconstruirRecursivo(Seq(), 0)
   }
-
-  def reconstruirCadenaTurbo(n: Int, o: Oraculo): Seq[Char] = {
-      def reconstruirCadenaTurboAux(n: Int, o: Oraculo): Seq[Seq[Char]] ={
-      var k = 1
-      var candidatos = alfabeto.map(Seq(_))
-      while (k < n) {
-        candidatos = candidatos.flatMap { candidato =>alfabeto.map(candidato :+ _)}
-        if (candidatos.isEmpty) Seq() //
-        k += 1
-      }
-      candidatos
-      }
-    def buscar_cadena(cadenasRestantes: Seq[Seq[Char]]): Seq[Char] = {
-      if (cadenasRestantes.isEmpty) {
-        Seq()
+  def reconstruirCadenaMejoradoPar(n: Int, o: Oraculo): Seq[Char] = {
+    def reconstruirRecursivo(actual: Seq[Char], longitudActual: Int): Seq[Char] = {
+      if (longitudActual == n && o(actual)) {
+        actual
+      } else if (longitudActual < n) {
+        val tareas = alfabeto.map(letra => task(reconstruirRecursivo(actual :+ letra, longitudActual + 1)))
+        val siguientes = tareas.flatMap(_.join())
+        siguientes
       } else {
-        val actual = cadenasRestantes.head
+        Seq()
+      }
+    }
+    reconstruirRecursivo(Seq(), 0)
+  }
+  def buscarCadena(cadenasRestantes: Seq[Seq[Char]], o: Oraculo): Seq[Char] = {
+    cadenasRestantes match {
+      case Nil => Seq()
+      case actual :: rest =>
         if (o(actual)) {
           actual
         } else {
-          buscar_cadena(cadenasRestantes.tail)
+          buscarCadena(rest,o)
+        }
+    }
+  }
+  def reconstruirCadenaTurbo(n: Int, o: Oraculo): Seq[Char] = {
+    def reconstruirCadenaTurboAux(k: Int, candidatos: Seq[Seq[Char]]): Seq[Seq[Char]] = {
+      if (k >= n) {
+        candidatos
+      } else {
+        val nuevosCandidatos = candidatos.flatMap { candidato =>
+          alfabeto.map(candidato :+ _)
+        }
+        reconstruirCadenaTurboAux(k + 1, nuevosCandidatos)
         }
       }
-    }
-    buscar_cadena(reconstruirCadenaTurboAux(n,o))
+    val candidatosIniciales = alfabeto.map(Seq(_))
+    buscarCadena(reconstruirCadenaTurboAux(1, candidatosIniciales),o)
   }
+  def reconstruirCadenaTurboPar(n: Int, o: Oraculo): Seq[Char] = {
+    def reconstruirCadenaTurboAuxPar(k: Int, candidatos: Seq[Seq[Char]]): Seq[Seq[Char]] = {
+      if (k >= n) {
+        candidatos
+      } else {
+        val tareas = candidatos.flatMap { candidato =>
+          alfabeto.map(letra => task(candidato :+ letra))
+        }
+        val nuevosCandidatos = tareas.map(_.join())
 
-
+        if (nuevosCandidatos.isEmpty) Seq()
+        else reconstruirCadenaTurboAuxPar(k + 1, nuevosCandidatos)
+      }
+    }
+    val candidatosIniciales = alfabeto.map(Seq(_))
+    val candidatosFinales = reconstruirCadenaTurboAuxPar(1, candidatosIniciales)
+    buscarCadena(candidatosFinales,o)
+  }
   def reconstruirCadenaTurboMejorada(n: Int, o: Oraculo): Seq[Char] = {
-    var k = 1
-    var candidatos = alfabeto.map(Seq(_))
+    @tailrec
+    def reconstruirCadena(k: Int, candidatos: Seq[Seq[Char]]): Seq[Seq[Char]] = {
+      if (k >= n) {
+        candidatos
+      } else {
+        val nuevosCandidatos = candidatos.flatMap { candidato =>
+          alfabeto.map(candidato :+ _)
+        }.filter(o)
 
-    while (k < n) {
-      candidatos = candidatos.flatMap { candidato =>
-        alfabeto.map(candidato :+ _)
-      }.filter(o) // Filtrar las cadenas candidatas con el oráculo
-
-      if (candidatos.isEmpty) Seq() // No hay cadenas válidas de longitud k
-
-      k += 1 // Duplicar la longitud para la próxima iteración
+        if (nuevosCandidatos.isEmpty) Seq()
+        else reconstruirCadena(k + 1, nuevosCandidatos)
+      }
     }
 
-    candidatos.find(_.length == n).getOrElse(Seq()) // Devolver la cadena de longitud n si existe, de lo contrario, una lista vacía
-    candidatos.headOption.getOrElse(Seq()) // Devolver la primera cadena si existe, de lo contrario, una lista vacía
+    val candidatosIniciales = alfabeto.map(Seq(_))
+    val candidatosFinales = reconstruirCadena(1, candidatosIniciales)
+
+    candidatosFinales.find(_.length == n).getOrElse(Seq())
   }
-
-
-
-  def main(args: Array[String]): Unit = {
-    // Se define el oráculo
-    val tamañoDeseado = 8
-    val secuencia_buscar = (1 to tamañoDeseado).map(_ => alfabeto(scala.util.Random.nextInt(alfabeto.length))).mkString("")
-
-    val tamaño_secuencia = secuencia_buscar.length
-    val o: Oraculo = (s: Seq[Char]) => {
-      secuencia_buscar.containsSlice(s)
-    }
+  def tiempo_funcion(n: Int, o: Oraculo, f: (Int, Oraculo) => Seq[Char]): Double = {
     val inicio = System.nanoTime()
-
-    println("Solucion ingenua")
-    println(s"Generando cadenas de tamaño: $tamaño_secuencia")
-    val cadenas = reconstuirCadenaIngenuo(tamaño_secuencia, o)
-    println(s"Cadena encontrada: $cadenas")
-
+    f(n, o)
     val fin = System.nanoTime()
-    println()
-
-    val tiempo1 = (fin - inicio) / 1e6
-
-    println("Solucion mejorada")
-
-    val inicio2 = System.nanoTime()
-    println(s"Generando cadenas de tamaño: $tamaño_secuencia")
-    val cadenas2 = reconstruirCadenaMejorado(tamaño_secuencia, o)
-    println(s"Cadena encontrada: $cadenas2")
-    val fin2 = System.nanoTime()
-    println()
-    val tiempo2 = (fin2 - inicio2) / 1e6
-
-    println("Solucion  Turbo")
-    val inicio3 = System.nanoTime()
-    println(s"Generando cadenas de tamaño: $tamaño_secuencia")
-    val cadenas3 = reconstruirCadenaTurbo(tamaño_secuencia, o)
-    println(s"Cadena encontrada: $cadenas3")
-    val fin3 = System.nanoTime()
-    println()
-    val tiempo3 = (fin3 - inicio3) / 1e6
-
-    println("Solucion  Turbo Mejorada")
-    val inicio4 = System.nanoTime()
-    println(s"Generando cadenas de tamaño: $tamaño_secuencia")
-    val cadenas4 = reconstruirCadenaTurboMejorada(tamaño_secuencia, o)
-    println(s"Cadena encontrada: $cadenas4")
-    val fin4 = System.nanoTime()
-    println()
-    val tiempo4 = (fin4 - inicio4) / 1e6
-
-    println(s"Tiempo de ejecución ingenuo:          $tiempo1 ms")
-    println(s"Tiempo de ejecución mejorado:         $tiempo2 ms")
-    println(s"Tiempo de ejecución turbo :           $tiempo3 ms")
-    println(s"Tiempo de ejecución turbo mejorado:   $tiempo4 ms")
-
+    (fin - inicio) / 1.0e9
+  }
+  def compararSecuencialParalela(funcionSecuencial: (Int, Oraculo) => Seq[Char], funcionParalela: (Int, Oraculo) => Seq[Char])(n: Int, o: Oraculo): (Double, Double, Double) = {
+    val tiempoSecuencial = tiempo_funcion(n, o, funcionSecuencial)
+    val tiempoParalelo = tiempo_funcion(n, o, funcionParalela)
+    val aceleracion = tiempoSecuencial / tiempoParalelo
+    (tiempoSecuencial, tiempoParalelo, aceleracion)
+  }
+  def main(args: Array[String]): Unit = {
+    for {
+      i <- 1 to 4
+      n = math.pow(2, i).toInt
+      secuencia = (1 to n).map(_ => alfabeto(Random.nextInt(alfabeto.length)))
+    } yield {
+        val o: Oraculo = (s: Seq[Char]) => secuencia.containsSlice(s)
+        val (tiempoSecuencial, tiempoParalelo, aceleracion) = compararSecuencialParalela(
+          reconstuirCadenaIngenuo, reconstuirCadenaIngenuoPar)(n, o)
+        print(s"n = $n  |")
+        print(s" $tiempoSecuencial  ")
+        print(s" $tiempoParalelo  ")
+        println(s" $aceleracion")
+    }
   }
 }
